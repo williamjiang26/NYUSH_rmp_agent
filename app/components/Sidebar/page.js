3266b865748a04f1c5d2a42d851b9eda6968c6af
William Jiang <williamjiang26@williams-air.mynetworksettings.com>
2024-08-26 00:39:46 -0400

    collapsed sidebar

--- app/components/Sidebar/page.js
+++ app/components/Sidebar/page.js
@@ -1,13 +1,17 @@
 import { Box, Button } from "@mui/material";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { useState } from "react";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+
 const ProSidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <Box sx={{ height: "100%", minHeight: "400px" }}>
-      <Sidebar collapsed={collapsed} collapsedWidth="100px">
+      <Sidebar collapsed={collapsed} collapsedWidth="80px" color="primary">
         <Button className="sb-button" onClick={() => setCollapsed(!collapsed)}>
-        Collapse
+          {collapsed ? <KeyboardArrowRightIcon/> : <KeyboardArrowLeftIcon/>}
+        
       </Button>
         <Menu>
           <MenuItem href="/chat">Chat</MenuItem>
